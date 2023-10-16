32d9c9da3d9a873553e00ad2561a79ecdf2a8286
thirukumaran05 <learningthiru@gmail.com>
2023-10-16 21:51:39 +0530

    Updated Controller

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,9 @@
-const random = require("../data/random.json");
+const randomApt = require("../data/random.json");
 const mixtureAndAlligation = require("../data/mixtureAndAlligation.json");
 const profitAndLoss = require("../data/profitAndLoss.json")
 
 exports.getBasics = (req, res, next) => {
-    res.status(200).json(random[(Math.round( Math.random() * (mixtureAndAlligation.length-1) ))]);
+    res.status(200).json(randomApt[(Math.round( Math.random() * (randomApt.length-1) ))]);
 };
 
 exports.getMixtureAndAlligation = (req, res, next) => {
@@ -11,5 +11,5 @@ exports.getMixtureAndAlligation = (req, res, next) => {
 }
 
 exports.getProfitAndLoss= (req, res, next) => {
-    res.status(200).json(profitAndLoss[(Math.round( Math.random() * (mixtureAndAlligation.length-1) ))])
+    res.status(200).json(profitAndLoss[(Math.round( Math.random() * (profitAndLoss.length-1) ))])
 }
