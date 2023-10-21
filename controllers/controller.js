ccbc435a87c3d91a3f465d8214e22426ce380126
thirukumaran05 <learningthiru@gmail.com>
2023-10-21 18:27:27 +0530

    Added Speed Time Distance

--- controllers/controller.js
+++ controllers/controller.js
@@ -2,6 +2,8 @@ const randomApt = require("../data/random.json");
 const mixtureAndAlligation = require("../data/mixtureAndAlligation.json");
 const profitAndLoss = require("../data/profitAndLoss.json");
 const age = require("../data/age.json");
+const permutationAndCombination = require("../data/PermutationAndCombination.json");
+const speedTimeDistance = require("../data/SpeedTimeDistance.json");
 
 exports.getBasics = (req, res, next) => {
     res.status(200).json(randomApt[(Math.round( Math.random() * (randomApt.length-1) ))]);
@@ -17,4 +19,14 @@ exports.getProfitAndLoss= (req, res, next) => {
 
 exports.getAge = (req, res, next) => {
     res.status(200).json(age[ (Math.random() * (age.length - 1)) ])
+}
+
+exports.getPermutationAndCombination = (req, res, next) => {
+    res.status(200).json(permutationAndCombination[(Math.round( Math.random() * (permutationAndCombination.length-1) ))])
+}
+
+exports.getSpeedTimeDistance =(req, res, next) => {
+    res.status(200).json(
+        speedTimeDistance[ Math.round( Math.random() * speedTimeDistance.length - 1 ) ]
+    )
 }
